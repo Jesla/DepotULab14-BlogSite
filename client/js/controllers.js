a7b464c9ddc48ea328d98540ca4effeb136dc422
Jesla <jesslar415@gmail.com>
2016-04-05 15:03:01 -0500

    listpost working

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,15 +1,5 @@
 var controllers = angular.module('myBlogApp');
 
-// controllers.controller('MainBlogCtrl', ['$scope', '$rootScope', '$http', '$location',
-//     function ($scope, $rootScope, $http, $location) {
-
-
-//         $scope.init = function () {
-//             $location.path('/listposts');
-//         };
-
-//     }]);
-
 controllers.controller('AllPostsCtrl', ['$scope', '$location', '$http', '$rootScope',
     function ($scope, $location, $http, $rootScope) {
 
@@ -17,30 +7,64 @@ controllers.controller('AllPostsCtrl', ['$scope', '$location', '$http', '$rootSc
             $scope.getPosts();
         }
 
-
         $scope.getPosts = function () {
             $http.get('http://localhost:3000/api/posts').then(function (data) {
-                // $scope.posts = data;
+                $scope.posts = data.data;
             }, function (err) {
                 console.error(err);
             });
         };
-        
-        $scope.goToCreate = function() {
-            console.log('inside goToCreate');
-             $location.path('/createpost');
+
+        $scope.goToCreate = function () {
+            $location.path('/createpost');
         }
 
-        
     }]);
-    
+
 controllers.controller('GoCreatePost', ['$scope', '$rootScope', '$http', '$location',
-        function ($scope, $rootScope, $http, $location) {
+    function ($scope, $rootScope, $http, $location) {
 
-            // $scope.goCreatePost = function () {
-            //     $location.url('/createpost');
-            // };
+        $scope.submitPost = function () {
+            var fullPost = {
+                title: $scope.titleField,
+                author: $scope.authorField,
+                content: $scope.contentField,
+            };
+            console.log(fullPost);
 
-}]);
+            $scope.titleField = '';
+            $scope.authorField = '';
+            $scope.contentField = '';
 
 
+            $http.post('http://localhost:3000/api/posts', fullPost).then(function (resp) {
+                console.log(resp);
+            }, function (err) {
+                console.error(err);
+            })
+
+            $location.path('/');
+            $http.get('http://localhost:3000/api/posts').then(function (data) {
+                $scope.posts = data.data;
+            }, function (err) {
+                console.error(err);
+            });
+        };
+    }]);
+
+
+controllers.controller('SinglePostCtrl', ['$scope', '$rootScope', '$http', '$location',
+    function ($scope, $rootScope, $http, $location) {
+
+        $scope.singleView = function (id) {
+            // $location.path("/singlepost");
+            // $http.get("http://localhost:3000/api/posts/#").then(function (data) {
+            //     $scope.posts = data.data;
+            // }, function (err) {
+            //     console.error(err);
+            // });
+            console.log("button works");
+
+        }
+
+    }]);
--- client/js/factories.js
+++ client/js/factories.js
@@ -1,6 +1,6 @@
 //The action to call the local API should be here
 
 // var app = angular.module('myBlogApp');
-// app.factory('', ['', []]);
+// app.factory('blogAppFactory', ['$http', []]);
 
 
